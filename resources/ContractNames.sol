acf63667f1a58903da06b24ccad7f7f0383cb7a2
ChunWang1998 <jjooee1998@gmail.com>
2025-07-14 22:52:21 +0800

    chore: contract name

--- resources/ContractNames.sol
+++ resources/ContractNames.sol
@@ -2,14 +2,43 @@
 pragma solidity 0.8.21;
 
 contract ContractNames {
-    string public constant SevenSeasRolesAuthorityName = "Seven Seas RolesAuthority Version 0.0";
-    string public constant EtherFiLiquidUsdRolesAuthorityName = "EtherFi Liquid USD RolesAuthority Version 0.0";
-    string public constant ArcticArchitectureLensName = "Arctic Architecture Lens V0.0";
-    string public constant EtherFiLiquidUsdName = "EtherFi Liquid USD V0.0";
-    string public constant EtherFiLiquidUsdManagerName = "EtherFi Liquid USD Manager With Merkle Verification V0.0";
-    string public constant EtherFiLiquidUsdAccountantName = "EtherFi Liquid USD Accountant With Rate Providers V0.0";
-    string public constant EtherFiLiquidUsdTellerName = "EtherFi Liquid USD Teller With Multi Asset Support V0.0";
-    string public constant EtherFiLiquidUsdDecoderAndSanitizerName = "EtherFi Liquid USD Decoder and Sanitizer V0.0";
-    string public constant AtomicQueueName = "Atomic Queue V0.0";
-    string public constant AtomicSolverName = "Atomic Solver V3.0";
+    // USDAI 
+    string public constant UsdaiDeployerName = "ccc SuperUSD Deployer V0.0";
+    string public constant UsdaiBoringOnChainQueuesRolesAuthorityName = "ccc SuperUSD Boring OnChain Queues Roles Authority V0.0";
+    string public constant UsdaiVaultRolesAuthorityName = "ccc SuperUSD Vault RolesAuthority V0.0";
+    string public constant UsdaiArcticArchitectureLensName = "ccc SuperUSD Arctic Architecture Lens V0.0";
+    string public constant UsdaiVaultName = "ccc SuperUSD Vault V0.0";
+    string public constant UsdaiVaultManagerName = "ccc SuperUSD Vault Manager With Merkle Verification V0.0";
+    string public constant UsdaiVaultAccountantName = "ccc SuperUSD Vault Accountant With Rate Providers V0.0";
+    string public constant UsdaiVaultTellerName = "ccc SuperUSD Vault Teller With Multi Asset Support V0.0";
+    string public constant UsdaiVaultDecoderAndSanitizerName = "ccc SuperUSD Vault Decoder and Sanitizer V0.0";
+    string public constant UsdaiVaultDelayedWithdrawer = "ccc SuperUSD Vault Delayed Withdrawer V0.0";
+    string public constant UsdaiVaultQueueName = "ccc SuperUSD Vault Queue V0.0";
+    string public constant UsdaiVaultQueueSolverName = "ccc SuperUSD Vault Queue Solver V0.0";
+    string public constant UsdaiLayerZeroTellerName = "ccc SuperUSD LayerZero Teller V0.0";
+    string public constant UsdaiChainlinkCCIPTellerName = "ccc SuperUSD Chainlink CCIP Teller V0.0";
+    string public constant UsdaiAcrossDecoderAndSanitizerName = "ccc SuperUSD Across Decoder and Sanitizer V0.0";
+    string public constant UsdaiMorphoDecoderAndSanitizerName = "ccc SuperUSD Morpho Decoder and Sanitizer V0.0";
+    string public constant UsdaiOpenEdenDecoderAndSanitizerName = "ccc SuperUSD OpenEden Decoder and Sanitizer V0.0";
+    string public constant UsdaiKyoDecoderAndSanitizerName = "ccc SuperUSD Kyo Decoder and Sanitizer V0.0";
+    string public constant UsdaiBaseDecoderAndSanitizerName = "ccc SuperUSD Base Decoder and Sanitizer V0.0";
+    string public constant UsdaiRoosterDecoderAndSanitizerName = "ccc SuperUSD Rooster Decoder and Sanitizer V0.0";
+    string public constant UsdaiEulerDecoderAndSanitizerName = "ccc SuperUSD Euler Decoder and Sanitizer V0.0";
+    string public constant UsdaiMerklDecoderAndSanitizerName = "ccc SuperUSD Merkl Decoder and Sanitizer V0.0";
+    
+    // sUSDAI 
+    string public constant sUsdaiBoringOnChainQueuesRolesAuthorityName = "ccc sSuperUSD Boring OnChain Queues Roles Authority V0.0";
+    string public constant sUsdaiVaultRolesAuthorityName = "ccc sSuperUSD Vault RolesAuthority V0.0";
+    string public constant sUsdaiArcticArchitectureLensName = "ccc sSuperUSD Arctic Architecture Lens V0.0";
+    string public constant sUsdaiVaultName = "ccc sSuperUSD Vault V0.0";
+    string public constant sUsdaiVaultManagerName = "ccc sSuperUSD Vault Manager With Merkle Verification V0.0";
+    string public constant sUsdaiVaultAccountantName = "ccc sSuperUSD Vault Accountant With Rate Providers V0.0";
+    string public constant sUsdaiVaultTellerName = "ccc sSuperUSD Vault Teller With Multi Asset Support V0.0";
+    string public constant sUsdaiVaultDecoderAndSanitizerName = "ccc sSuperUSD Vault Decoder and Sanitizer V0.0";
+    string public constant sUsdaiVaultDelayedWithdrawer = "ccc sSuperUSD Vault Delayed Withdrawer V0.0";
+    string public constant sUsdaiVaultQueueName = "ccc sSuperUSD Vault Queue V0.0";
+    string public constant sUsdaiVaultQueueSolverName = "ccc sSuperUSD Vault Queue Solver V0.0";
+    string public constant sUsdaiLayerZeroTellerName = "ccc sSuperUSD LayerZero Teller V0.0";
+    string public constant sUsdaiChainlinkCCIPTellerName = "ccc sSuperUSD Chainlink CCIP Teller V0.0";
+    string public constant sUsdaiBaseDecoderAndSanitizerName = "ccc sSuperUSD Base Decoder and Sanitizer V0.0";
 }
